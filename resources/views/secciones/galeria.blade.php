75773872b3a0b2521314cd8db989f21c213a6c2f
Carlos Quintero <admin.fusion@gmail.com>
2022-08-04 12:00:11 -0500

    pa galeria

--- resources/views/secciones/galeria.blade.php
+++ resources/views/secciones/galeria.blade.php
@@ -1,7 +1,7 @@
 @if($cliente->galeria->count() > 0)
-<section id="galeria" class="mt-5 px-5 text-center lg:mt-10">
-	<div class="text-center color text-4xl lg:text-8xl">Así se vive</div>
-	<div class="text-center text-4xl font-extrabold lg:text-8xl">{{ $cliente->titulo }}</div>
+<section id="galeria" class="mt-5 text-center lg:mt-10">
+	<div class="text-center px-5 color text-4xl lg:text-8xl">Así se vive</div>
+	<div class="text-center px-5 text-4xl font-extrabold lg:text-8xl">{{ $cliente->titulo }}</div>
 	<div id="galeria-swiper" class="swiper swiper-galeria mt-5 lg:mt-10">
 		<!-- Additional required wrapper -->
 		<div class="swiper-wrapper pb-14">
