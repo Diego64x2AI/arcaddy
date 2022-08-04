6e315bcab6c514946114bbeee90e1e2b7f48e23f
Carlos Quintero <admin.fusion@gmail.com>
2022-08-04 11:57:15 -0500

    patrocinadores padding

--- resources/views/secciones/patrocinadores.blade.php
+++ resources/views/secciones/patrocinadores.blade.php
@@ -1,5 +1,5 @@
 @if($cliente->patrocinadores->count() > 0)
-<section id="patrocinadores" class="mt-5 py-5 text-center max-w-5xl mx-auto lg:px-8 lg:mt-10">
+<section id="patrocinadores" class="mt-5 p-5 text-center max-w-5xl mx-auto lg:px-8 lg:mt-10">
 	<div class="text-center font-extrabold text-4xl lg:text-8xl">Patrocinadores</div>
 	<div class="color text-center font-extrabold text-4xl lg:text-8xl">{{ $cliente->titulo }}:</div>
 	<div class="flex flex-col flex-wrap lg:flex-row items-center">
