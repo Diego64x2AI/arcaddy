654110b3f1678ced74946c7e2a2de7aee23ef95c
Carlos Quintero <admin.fusion@gmail.com>
2022-11-04 16:39:22 -0600

    video autoplay

--- resources/views/secciones/live.blade.php
+++ resources/views/secciones/live.blade.php
@@ -4,7 +4,7 @@
 	@endphp
 	{{-- https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcarlos.fusion%2Fvideos%2F386377026809862%2F&width=1280 --}}
 	<iframe
-		src="https://www.youtube.com/embed/{{ $matches[0] }}"
+		src="https://www.youtube.com/embed/{{ $matches[0] }}?&autoplay=1&mute=1"
 		frameborder="0"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 		allowfullscreen
