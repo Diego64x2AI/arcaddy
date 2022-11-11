273792a15ebb1cfc75659280ff522d62b8bc6951
Carlos Quintero <admin.fusion@gmail.com>
2022-11-11 01:47:13 -0600

    no titulos

--- resources/views/secciones/galeria.blade.php
+++ resources/views/secciones/galeria.blade.php
@@ -1,6 +1,6 @@
 @if($cliente->galeria->count() > 0)
 <section id="galeria" class="mt-5 text-center lg:mt-10">
-	<div class="text-center px-5 text-4xl font-extrabold lg:text-8xl">{{ $cliente->titulo }}</div>
+	{{-- <div class="text-center px-5 text-4xl font-extrabold lg:text-8xl">{{ $cliente->titulo }}</div> --}}
 	<div id="galeria-swiper" class="swiper swiper-galeria mt-5 lg:mt-10">
 		<!-- Additional required wrapper -->
 		<div class="swiper-wrapper pb-14">
--- resources/views/secciones/patrocinadores.blade.php
+++ resources/views/secciones/patrocinadores.blade.php
@@ -1,7 +1,7 @@
 @if($cliente->patrocinadores->count() > 0)
 <section id="patrocinadores" class="mt-5 p-5 text-center max-w-5xl mx-auto lg:px-8 lg:mt-10">
 	<div class="text-center font-extrabold text-4xl lg:text-8xl">Patrocinadores</div>
-	<div class="color text-center font-extrabold text-4xl lg:text-8xl">{{ $cliente->titulo }}:</div>
+	{{--  <div class="color text-center font-extrabold text-4xl lg:text-8xl">{{ $cliente->titulo }}:</div>--}}
 	<div class="flex flex-col flex-wrap lg:flex-row items-center">
 		@foreach($cliente->patrocinadores as $patrocinador)
 		<div class="w-full md:w-1/2 px-2 mt-10">
