15d0eba576049cf43a290e633a7e7d444bff8920
Carlos Quintero <admin.fusion@gmail.com>
2022-11-15 17:26:34 -0600

    login link register fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,7 +58,7 @@ $classes = $cliente->id === NULL ? 'degradado' : 'bg-gray-100';
 				</div>
 			@endforeach
 			<div class="flex items-center justify-end mt-4">
-				<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+				<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login', ['cliente' => $cliente->id]) }}">
 					{{ __('Already registered?') }}
 				</a>
 				<x-button class="ml-4 btn-pill">
