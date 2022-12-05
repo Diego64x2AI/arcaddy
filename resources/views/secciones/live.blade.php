1e37a23b8e2cb4448db51ddbe3f8342b9ccecd2b
Carlos Quintero <admin.fusion@gmail.com>
2022-12-05 12:39:57 -0600

    aspect ratio video

--- resources/views/secciones/live.blade.php
+++ resources/views/secciones/live.blade.php
@@ -2,12 +2,14 @@
 	@php
 	preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $cliente->facebook_live, $matches);
 	@endphp
-	{{-- https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcarlos.fusion%2Fvideos%2F386377026809862%2F&width=1280 --}}
-	<iframe
-		src="https://www.youtube.com/embed/{{ $matches[0] }}?&autoplay=1&mute=1"
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-		class="w-full aspect-video"
-	></iframe>
+	<div class="aspect-w-16 aspect-h-9">
+		{{-- https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcarlos.fusion%2Fvideos%2F386377026809862%2F&width=1280 --}}
+		<iframe
+			src="https://www.youtube.com/embed/{{ $matches[0] }}?&autoplay=1&mute=1"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			class="w-full aspect-video"
+		></iframe>
+	</div>
 @endif
