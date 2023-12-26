48bf3b35ac3f4732922f372dfc50ef1247f4f402
José Alejandro Sánchez Rojas <Beloki@MacBook-Pro-de-Jose-2.local>
2023-12-26 15:59:44 -0600

    Bloqueo de opciones la menu de my app para el cliente pananews

--- resources/views/my-app-client/home.blade.php
+++ resources/views/my-app-client/home.blade.php
@@ -1,31 +1,37 @@
 @extends('my-app-client.layout')
 
 @section('content')
+@if(isset($clientedatos))
 <div class="alx-body-padding">
 	<div class="alx-mobile-int" style="padding-left: 5px;padding-right: 5px;">
 		<a class="alx-home-btn" id="alx-btn-ingreso"  href="{{route('my-app-client.check-in')}}">
 			Ingreso de <br>usuarios a evento
 		</a>
 	</div>	
-	<div class="alx-mobile-int">
+	
 		
 <?php /*
 		<a class="alx-home-btn" id="alx-btn-activaciones" href="{{route('my-app-client.reporte-activaciones')}}">
 			Activaciones AR <br>y códigos QR
 		</a>*/?>
-
+	@if($clientedatos->id != 86)
+	<div class="alx-mobile-int">
 		<a class="alx-home-btn" id="alx-btn-redenciones" href="{{route('my-app-client.reporte-redenciones')}}">
 			Redenciones <br>digitales
 
 		</a>
-
+	</div>
+	<div class="alx-mobile-int">
 		<a class="alx-home-btn" id="alx-btn-usuarios" href="{{route('my-app-client.reporte-base-usuarios')}}">
 			Base de datos <br>de usuarios
 		</a>
+	</div>
+	@endif
 <?php /*
 		<a class="alx-home-btn" id="alx-btn-estadisticas" href="{{route('my-app-client.reporte-estadisticas')}}">
 			Estadísticas <br>generales
 		</a>*/?>
-	</div>
+	
 </div>
+@endif
 @endsection
