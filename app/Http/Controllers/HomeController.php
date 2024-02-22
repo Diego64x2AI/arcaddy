c3cafbafaa45c3a2da9e3b827f5cbef9df89c448
José Alejandro Sánchez Rojas <Beloki@MacBook-Pro-de-Jose-2.local>
2024-02-22 09:10:23 -0600

    Se agrego stich betterware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -168,6 +168,10 @@ END:VCALENDAR";
 			header("Location: https://pananews.panama.com.mx/panama");
 			exit();
 		}
+		elseif (strpos($_SERVER['HTTP_HOST'], 'stitch.betterware.com.mx') !== false && $slug !== 'stitch') {
+			header("Location: https://stitch.betterware.com.mx/stitch");
+			exit();
+		}
 		/*elseif(strpos($_SERVER['HTTP_HOST'], 'oce-eg-ra.mx') !== false && $slug == 'gelicart'){
 
 	    }*/
