eb2863a65d5470079a39d6b0818b7a091a875dc3
José Alejandro Sánchez Rojas <Beloki@MacBook-Pro-de-Jose-2.local>
2024-03-01 12:15:02 -0600

    Se agrego chandrany

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -180,6 +180,10 @@ END:VCALENDAR";
 			header("Location: https://stitch.betterware.com.mx/stitch");
 			exit();
 		}
+		elseif (strpos($_SERVER['HTTP_HOST'], 'chandrany.com') !== false && $slug !== 'chandrany') {
+			header("Location: https://chandrany.com/chandrany");
+			exit();
+		}
 		/*elseif(strpos($_SERVER['HTTP_HOST'], 'oce-eg-ra.mx') !== false && $slug == 'gelicart'){
 
 	    }*/
