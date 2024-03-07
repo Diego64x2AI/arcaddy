c78cc59454dc9611b58aabe581902ac2a81a9316
Carlos Quintero <admin.fusion@gmail.com>
2024-03-06 19:08:18 -0600

    cerrar otros

--- resources/views/secciones/menu.blade.php
+++ resources/views/secciones/menu.blade.php
@@ -51,8 +51,11 @@
 <script>
 	window.addEventListener('load', function() {
 		$('.accordeon-link').click(function(){
+			// close other accordions
+			$('.accordeon-link').not(this).removeClass('open').find('i').removeClass('fa-minus').addClass('fa-plus');
+			$('.accordeon-link').not(this).next().slideUp();
 			$(this).next().slideToggle();
-			$(this).find('i').toggleClass('fa-plus fa-minus');
+			$(this).toggleClass('open').find('i').toggleClass('fa-plus fa-minus');
 		});
 		$('.menu-item-link').click(function(e) {
 			e.preventDefault();
