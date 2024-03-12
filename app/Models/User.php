bb93205e50351f522a8a5c49bae33cdab2d3efce
Carlos Quintero <admin.fusion@gmail.com>
2024-03-11 18:53:26 -0600

    export

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,11 @@ class User extends Authenticatable
 		return $this->hasMany(ClienteUserFieldValue::class,  'user_id', 'id');
 	}
 
+	// user qr
+	public function qr() {
+		return $this->hasOne(UserQr::class, 'user_id', 'id');
+	}
+
 	/**
 	 * The "booted" method of the model.
 	 *
