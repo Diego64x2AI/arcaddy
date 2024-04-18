76845cbcccdc89af2c84a9c92de3d30f30138998
Carlos Quintero <admin.fusion@gmail.com>
2024-04-17 20:10:58 -0600

    fix link color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -85,7 +85,7 @@
 
 			<div class="flex items-center justify-end mt-4">
 				@if (Route::has('password.request'))
-				<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request', ['cliente' => $cliente->id]) }}">
+				<a class="underline text-sm color" href="{{ route('password.request', ['cliente' => $cliente->id]) }}">
 					{{ __('Forgot your password?') }}
 				</a>
 				@endif
