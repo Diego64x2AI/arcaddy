db7a26c213966f2e358d58e15ae528ef50d5ecc3
Carlos Quintero <admin.fusion@gmail.com>
2024-04-27 15:48:42 -0600

    fix otra quiz english

--- resources/views/secciones/quiz/option.blade.php
+++ resources/views/secciones/quiz/option.blade.php
@@ -3,7 +3,7 @@
 	<div>
 		<input type="radio" class="mr-2" id="respuesta-{{ $respuesta->id }}" name="respuesta-{{ $pregunta->id }}" data-otra="{{ ($respuesta->respuesta === 'Otra...') ? 1 : 0 }}" value="{{ $respuesta->id }}">
 		@if ($respuesta->respuesta === 'Otra...')
-		<label for="respuesta-{{ $respuesta->id }}"><input type="text" name="respuesta-{{ $pregunta->id }}-otra" placeholder="{{ str_replace("", __('arcaddy.other'), $respuesta->respuesta) }}" class="border py-2 text-sm border-gray-300 w-3/4 color"></label>
+		<label for="respuesta-{{ $respuesta->id }}"><input type="text" name="respuesta-{{ $pregunta->id }}-otra" placeholder="{{ str_replace("Otra...", __('arcaddy.other'), $respuesta->respuesta) }}" class="border py-2 text-sm border-gray-300 w-3/4 color"></label>
 		@else
 		<label for="respuesta-{{ $respuesta->id }}">{{ $respuesta->respuesta }}</label>
 		@endif
