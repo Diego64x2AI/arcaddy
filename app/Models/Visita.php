20b171230f83609a9abb1bc566f9252df12ecc33
Carlos Quintero <admin.fusion@gmail.com>
2024-12-13 13:44:03 -0600

    visitas

---
+++ app/Models/Visita.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visita extends Model
+{
+  use HasFactory;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'ip',
+		'iso_code',
+		'country',
+		'city',
+		'state',
+		'state_name',
+		'timezone',
+		'continent',
+		'so',
+		'language',
+		'browser',
+		'lon',
+		'lat',
+		'url',
+		'model',
+		'model_id',
+		'user_id',
+	];
+
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [];
+
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime',
+	];
+
+	protected $with = [];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+}
